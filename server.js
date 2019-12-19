4ea9f49cf2481199542e86c179a4ad0d997f841f
slighter12 <slighter12@slighter12de-MacBook-Pro.local>
2019-12-19 10:29:29 +0800

    fix bug in server.js

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const express = require('express');
 const compression = require('compression');
 
 const app = express(),
-            DIST_DIR = __dirname,
-            HTML_FLIE = path.join(DIST_DIR, 'index.html');
+    DIST_DIR = __dirname,
+    HTML_FILE = path.join(DIST_DIR, 'index.html');
 const PORT = process.env.PORT || 8080;
 
 app.use(compression());
-app.use(express.static(DIST_DIR));
+app.use(express.json());
+app.use('/', express.static(path.join(DIST_DIR, 'dist')));
 
 app.get('/', (req, res) => {
     res.sendFile(HTML_FILE);
