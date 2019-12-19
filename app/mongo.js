48f825cabf747026e3b54f385e49a3518d6810a5
slighter12 <slighter12@slighter12de-MacBook-Pro.local>
2019-12-19 11:39:03 +0800

    initial setting about mongoDB

---
+++ app/mongo.js
@@ -0,0 +1,24 @@
+const MongoClient = require('mongodb').MongoClient,
+    dburi = process.env.MONGODB_URI;
+
+async function upsertDb(key, data, dbName) {
+    let client;
+    let url = `${dburi}/'${dbname}'`;
+    try {
+        client = await MongoClient.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true });
+        const db = client.db();
+        let mongoOps = [{
+            updateOne: {
+                filter: key,
+                update: data,
+                upsert: true
+            }
+        }];
+        db.collection(dbName).bulkWrite(mongoOps, err => { if (err) throw err });
+    } catch (err) {
+        console.log(err.stack);
+    }
+    if (client) {
+        await client.close();
+    }
+}
